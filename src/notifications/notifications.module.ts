506837279a96097c5217800d70fd3f7dc0fc8295
TheJat <manishkl543@gmail.com>
2026-05-19 06:51:29 +0530

    fix: notification service is not receiving the message because config package is not installed, and after migrating to the cloud amqp it is still checking for the localhost because reading from the .env is giving the 'undefined' as package was not installed

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,9 +3,15 @@ import { Module } from '@nestjs/common';
 import { EventsModule } from 'src/core/events/events.module';
 
 import { UserCreatedConsumer } from './consumers/user-created.consumer';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    EventsModule,
+    ConfigModule.forRoot({
+        isGlobal: true,
+    }),
+],
 
   providers: [
     UserCreatedConsumer,
